642c7cc8f396508ce0deae04c6a117548f6579b2
Marianne Cordova <macoam00@hotmail.com>
2021-03-10 14:32:03 -0700

    100321

---
+++ readframes.py
@@ -0,0 +1,13 @@
+import wave
+import numpy as np
+
+
+goodmorning = wave.open('good-morningMan.wav', 'r')
+
+frames = goodmorning.readframes(-1)
+
+print(frames[:10])
+
+ondaconvertida = mp.frombuffer (frames, dtype='int16')
+
+print (ondaconvertida[:10])
