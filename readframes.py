e0f35240aba7bab29efb2a79a723e6d4ee180b96
Marianne Cordova <macoam00@hotmail.com>
2021-03-10 14:32:52 -0700

    2.0

--- readframes.py
+++ readframes.py
@@ -8,6 +8,6 @@ frames = goodmorning.readframes(-1)
 
 print(frames[:10])
 
-ondaconvertida = mp.frombuffer (frames, dtype='int16')
+ondaconvertida = np.frombuffer (frames, dtype='int16')
 
 print (ondaconvertida[:10])
